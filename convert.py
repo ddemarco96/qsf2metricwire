bc3354310ecf56b89fb15835df23d42a488e241d
Dylan DeMarco <ddemarco@college.harvard.edu>
2022-10-10 16:44:14 -0400

    First full pass at basic conversion

--- convert.py
+++ convert.py
@@ -3,6 +3,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import json
+import re
+
 import pandas as pd
 from bs4 import BeautifulSoup
 
@@ -41,18 +43,62 @@ def convert_qsf():
                 question['recoded'] = True
                 choice_dict = item['Payload']['RecodeValues']
                 question['choices'] = "|".join([choice['Display'] for choice in choice_dict.values()])
+
+            if question['qualtrics_type'] == "MC":
+                choice_str = question['choices']
+                # matches 1|2|3|4|5 and 1 - good|2|3 - bad; but not pizza|salad|sandwich
+                likert_pat = "^\d+\s?-?\s?[a-zA-Z\s]*\|?$"
+                if re.fullmatch(likert_pat, choice_str):
+                    question['qualtrics_type'] = "SLIDING_SCALE"
+                anchor_pat = "-\s?[a-zA-Z\s]+"
+                anchors = re.findall(anchor_pat, choice_str)
+                if len(anchors) == 2:
+                    # sometimes the Nock lab likes to write the anchor labels in the choices, e.g., "1 - bad"
+                    # If so, grab the anchor and move it to the MW label.
+                    # don't for get to slice off the "- "
+                    question['startLabel'] = anchors[0][1:].strip()  # strip not slice in case there isn't a space
+                    question['endLabel'] = anchors[1][1:].strip()
+                    question['choices'] = question['choices'].replace(f" {anchors[0]}", "")
+                    question['choices'] = question['choices'].replace(f" {anchors[1]}", "")
+
             questions.append(question)
-    import pdb; pdb.set_trace()
-    qualtrics_types = {
-        "MC": "Multiple Choice",
-        "TE": "Text Entry",
-        "DB": "Descriptive Box",
-        "Matrix": "Matrix",
-        "SBS": "Side by side",
-        "DD": "Drop-down",
-    }
+
     print(f"Found {len(questions)} Questions in QSF")
 
+    # Create a dataframe in MetricWire format from the information pulled
+    df = pd.DataFrame(questions)
+    df.rename({
+        'text': 'question',
+        'label': 'variableName',
+        'qualtrics_type': 'type',
+    }, axis=1, inplace=True)
+
+    supported_types = ["MC", "TE", "DB", "SLIDING_SCALE"]
+    used_types = df.loc[~df['type'].isin(supported_types)]
+    if not df.loc[~df['type'].isin(supported_types)].empty:
+        print("This script only supports: single-choice, text entry, and info text questions from Qualtrics currently.")
+
+
+    qtype_to_mwtype = {
+        "MC": "SINGLE_CHOICE",  # "Multiple Choice",
+        "TE": "TEXT",  # "Text Entry",
+        "DB": "INFORMATION",  # "Descriptive Box",
+        # "Matrix": "Matrix", # Not sure what to do with these yet
+        # "SBS": "Side by side",
+        "DD": "SINGLE_CHOICE",  # "Drop-down",
+    }
+
+
+    df.replace({"type": qtype_to_mwtype}, inplace=True)
+
+
+    needed_cols = pd.read_csv('./mw_template.csv').columns
+    for col in needed_cols:
+        if col not in df.columns:
+            df[col] = None
+
+    df.fillna("", inplace=True)
+    df.to_csv("convert_qsf.csv", index=False)
 
     """
     Qualtrics fields
